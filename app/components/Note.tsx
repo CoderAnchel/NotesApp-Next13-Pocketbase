a42168b327b3b743418468ef204aa1d4f29715e6
CoderAnchel <alex21fd@gmail.com>
2022-11-05 11:35:57 +0100

    🚀

---
+++ app/components/Note.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import styles from './Notes.module.css';
+
+export default function Note({note}: any) {
+    const {id, title, content, created } = note || {};
+
+    return (
+        <div>
+            <Link href={`/notes/${id}`}>
+                <div className={styles.note}>
+                    <h2>{title}</h2>
+                    <h5>{content}</h5>
+                    <p>{created}</p>
+                </div>
+            </Link>
+        </div>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Notes Page using Next13 + Pocket Base</p>
+    </div>
+  )
+}
+
